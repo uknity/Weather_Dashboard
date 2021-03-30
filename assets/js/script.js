4e502eb245dd5eb3731000f24254a6ebcca2970a
Liz Emerson <uknity@gmail.com>
2021-03-30 09:39:10 -0400

    pulled uvAPI and referenced in fetch command

--- assets/js/script.js
+++ assets/js/script.js
@@ -70,6 +70,13 @@ function getCity() {
 
   var fiveDayForecastData = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=cc1d108015a60ec6fb2d04e49e6039dd`;
 
+  var uvApi = `https://http://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=cc1d108015a60ec6fb2d04e49e6039dd`;
+
+  fetch(uvApi)
+    .then(function (response) {
+        return response.json();
+    })
+
   fetch(fiveDayForecastData)
     .then(function (response) {
       return response.json();
@@ -170,34 +177,4 @@ searchButton.on("click", function (event) {
   getCity();
 });
 
-// fetchButton.addEventListener('click', getApi);
 
-// function getApi() {
-
-//     var requestUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.753746&lon=-84.386330&exclude=alerts&appid=cc1d108015a60ec6fb2d04e49e6039dd";
-
-//     fetch(requestUrl)
-//       .then(function (response) {
-//         return response.json();
-//       })
-//       .then(function (data) {
-//           console.log(data);
-//           for (var i = 0; i < data.length; i++) {
-//               // Creating elements, tablerow, tabledata, and anchor
-//               var createTableRow = document.createElement('tr');
-//               var tableData = document.createElement('td');
-//               var link = document.createElement('a');
-
-//               // Setting the text of link and the href of the link
-//               link.textContent = data[i].html_url;
-//               link.href = data[i].html_url;
-
-//               // Appending the link to the tabledata and then appending the tabledata to the tablerow
-//               // The tablerow then gets appended to the tablebody
-//               tableData.appendChild(link);
-//               createTableRow.appendChild(tableData);
-//               tableBody.appendChild(createTableRow);
-//             }
-
-//       });
-//   }
